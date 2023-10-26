ef56055bbc5aa3794ecfeb19f618c0b49d3f89f6
Jon Renslo <jonrenslo@gmail.com>
2023-10-26 05:50:49 -0700

    added installation script

---
+++ runme.sh
@@ -0,0 +1,9 @@
+cd $HOME
+#ln -sb dotfiles/.screenrc .
+ln -sb dotfiles/.bash_profile .
+ln -sb dotfiles/.bashrc .
+ln -sb dotfiles/.bashrc_custom .
+#mv .emacs.d .emacs.d~
+#ln -s dotfiles/.emacs.d .
+ln -sb dotfiles/my.gitconfig ./.gitconfig
+ln -sb dotfiles/.vimrc .
